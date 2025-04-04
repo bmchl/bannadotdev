6cf74e89c48e64b50e304b9081594df16f18f913
Michael Banna <90261090+bmchl@users.noreply.github.com>
2025-04-03 22:03:53 -0400

    repo clean up, bio rewrite, design refresh

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,29 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-    './node_modules/flowbite/**/*.js',
-  ],
-  theme: {
-    extend: {},
-    colors: {
-      transparent: 'transparent',
-      backdrop: '#F8F1EB',
-      current: 'currentColor',
-      writing: {
-        DEFAULT: '#0D0C1D',
-        light: '#0D0C1D',
-        dark: '#F4E7D4',
-      },
-      primary: {
-        DEFAULT: '#474973',
-        light: '#A69CAC',
-        ultralight: '#E8E0DE',
-        dark: '#161B33',
-      },
-      secondary: '#F1DAC4',
+    content: [
+        "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            transparent: "transparent",
+            backdrop: "#F8F1EB",
+            current: "currentColor",
+            writing: {
+                DEFAULT: "#0D0C1D",
+                light: "#0D0C1D",
+                dark: "#F4E7D4",
+            },
+            primary: {
+                DEFAULT: "#474973",
+                light: "#A69CAC",
+                ultralight: "#E8E0DE",
+                dark: "#161B33",
+            },
+            secondary: "#F1DAC4",
+        },
     },
-  },
-  plugins: [require('flowbite/plugin')],
-  import: ['src/fonts.css'],
+    plugins: [require("flowbite/plugin")],
 };
